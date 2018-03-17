e335bdd89dd2c8e3e575ca3c20f7b3af23f00578
Xian Zheng <xzheng2112@gmail.com>
2018-03-16 18:23:44 -0600

    BlackjackTableApp busted() for each player

--- src/com/skilldistillery/cards/blackjack/BlackjackTableApp.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackTableApp.java
@@ -20,9 +20,9 @@ public class BlackjackTableApp {
 		Hand playerHand = player.getHand();
 		Dealer dealer = new Dealer();
 		Hand dealerHand = dealer.getHand();
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("~~~~~Welcome to the Blackjack Table~~~~~");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~ Welcome to the Blackjack Table ~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 
 		playerHand.addCard(newDeck.dealCard());
 		playerHand.addCard(newDeck.dealCard());
@@ -32,7 +32,9 @@ public class BlackjackTableApp {
 		dealerHand.addCard(newDeck.dealCard());
 		System.out.println("The dealer's " + dealerHand);
 		
-		winner(playerHand, dealerHand);
+		if(winner(playerHand, dealerHand) == true) {
+			System.exit(0);
+		}
 		hitStay(playerHand, newDeck, kb);
 		dealerHitStay(dealerHand, newDeck);
 
@@ -50,37 +52,60 @@ public class BlackjackTableApp {
 				System.out.println("Your " + playerHand);
 				hitStay(playerHand, newDeck, kb);
 
+				playerBust(playerHand);
 			}
 
 			else if (hitOrStay == 2) {
 				System.out.println("\nYour final hand is: " + playerHand);
 			}
 		}
-		bust(playerHand);
 	}
 
 	public static void dealerHitStay(Hand dealerHand, Deck newDeck) {
 		while (dealerHand.getValueOfHand() < 17) {
 			dealerHand.addCard(newDeck.dealCard());
 		}
-		bust(dealerHand);
+		dealerBust(dealerHand);
 		System.out.println("The dealer's " + dealerHand);
 	}
 
-	private static void bust(Hand hand) {
-		if (hand.getValueOfHand() > 21) {
-			System.out.println("\nYou have busted! The " + hand);
+	private static void playerBust(Hand playerHand) {
+		if (playerHand.getValueOfHand() > 21) {
+			System.out.println("\nYou Busted!");
 		}
 
 	}
 
-	private static void winner(Hand playerHand, Hand dealerHand) {
-		if (playerHand.getValueOfHand() == 21) {
-			System.out.println("\nCongratulations you have won!");
-		}
-		if (dealerHand.getValueOfHand() == 21) {
-			System.out.println("\nThe dealer won!");
+	private static void dealerBust(Hand dealerHand) {
+		if (dealerHand.getValueOfHand() > 21) {
+			System.out.println("\nThe Dealer Busted!");
 		}
 
+	}
+
+	private static boolean winner(Hand playerHand, Hand dealerHand) {
+		if (playerHand.getValueOfHand() == 21) {
+			System.out.println("\nCongratulations you have won!");
+			return true;
+		}
+		else if (dealerHand.getValueOfHand() == 21) {
+			System.out.println("\nThe dealer won!");
+			return true;
+		}
+		
+		else if ((dealerHand.getValueOfHand() > playerHand.getValueOfHand()) 
+				&& (dealerHand.getValueOfHand() <= 21) 
+				&& (dealerHand.getValueOfHand() >= 17)) {
+			System.out.println("\nThe dealer has won!");
+			return true;
+		}
+		
+		else if ((playerHand.getValueOfHand() > dealerHand.getValueOfHand()) 
+				&& (playerHand.getValueOfHand() <= 21)
+				&& (playerHand.getValueOfHand() >= 17)) {
+			System.out.println("\nYou have won!");
+			return true;
+		}
+		return false;
 	}
 }
